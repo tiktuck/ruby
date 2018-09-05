9e12532891f2c63d5c344049b094ae36fd34521b
khoa <student.bkhn@gmail.com>
2018-09-05 23:00:47 +0700

    lbp

--- block.rb
+++ block.rb
@@ -1,14 +1,34 @@
-#!/usr/bin/ruby
+a = (1..10).each { |e| puts e*2 }
+
+p = Proc.new { puts 'This is proc' }
+p.call
+p.class
+
+l = -> (x) { puts x}
+a = [1,2,3]
+l.call(a)
 
 def block
-	puts "One"
-	yield
-	yield 3
-	puts "Two"
-	yield
-	yield 100
+	puts 'Calling block'
+	yield(10)
 end
 
-block { puts "This is a block"}
+block { |x| 
+	puts x*x*x 
+	puts 'End block'
+}
 
-block {|v| puts "This is in block #{v}"}
+h = {}
+
+h['a'] = 100
+h['b'] = 50
+h['c'] = 10
+h[1] = 1
+h[1] = 100
+h[:a] = 5
+h.each do |k, val|
+	puts "h[#{k}] = #{val}"	
+end
+
+h.map { |k, v|  h[k] = v*2 }
+puts h
